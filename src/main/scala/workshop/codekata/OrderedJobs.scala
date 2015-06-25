184f1aa69e6f880ac97c90c6e4924ebf2146ef55
Teqqles <david.long@racing-leagues.co.uk>
2015-06-25 13:40:55 +0100

    Implemented OrderedList kata code

--- src/main/scala/workshop/codekata/OrderedJobs.scala
+++ src/main/scala/workshop/codekata/OrderedJobs.scala
@@ -1,15 +1,50 @@
 package workshop.codekata
 
-class JobSelfReferenceException extends Exception
+class JobSelfReferenceException extends RuntimeException( "Jobs can't depend on themselves", null )
 
-class JobCircularDependencyException extends Exception
+class JobCircularDependencyException extends RuntimeException( "Jobs can't have circular dependencies", null )
+
+case class Job( id: String, dependency: String )
 
 object OrderedJobs {
 
-    def parse( str: String ) = List()
+    def addJob( list: List[ String ], job: Job ): List[ String ] = {
+        def insertBefore( x: String, y: String ): List[ String ] = {
+            ( list.slice( 0, list.indexOf( x ) ) :+ y ) ::: list.drop( list.indexOf( x ) )
+        }
 
+        job match {
+            case Job( x, null ) if !list.contains( x ) => list :+ x
+            case Job( x, y: Any ) if !list.contains( x ) && !list.contains( y ) => list :+ y :+ x
+            case Job( x, y: Any ) if !list.contains( y ) => insertBefore( x, y )
+            case Job( x, y: Any ) if !list.contains( x ) => list :+ x
+            case _ => list
+        }
+    }
+
+    def parse( str: String ): List[ String ] = {
+        val list: List[ Job ] = str match {
+            case "" => List( )
+            case _ => str.split( "\n" ).toList.map( x => JobExtractor( x ) )
+        }
+
+        def extractJobs( jobsRemaining: List[ Job ], jobsList: List[ String ] ): List[ String ] = {
+            jobsRemaining match {
+                case x :: _ if jobsList.contains( x.id ) && jobsList.contains( x.dependency ) =>
+                    throw new JobCircularDependencyException
+                case x :: _ => extractJobs( jobsRemaining.tail,
+                    addJob( jobsList, x ) )
+                case Nil => jobsList
+            }
+        }
+        extractJobs( list, List( ) )
+    }
+
+    private def JobExtractor( job: String ): Job = {
+        job.split( " =>.?" ) match {
+            case Array( x, y ) if x == y => throw new JobSelfReferenceException
+            case Array( x, y ) => Job( x, y )
+            case Array( x ) => Job( x, null )
+        }
+    }
 }
-
-
-
-
